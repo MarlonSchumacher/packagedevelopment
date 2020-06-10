324871883faa2fabc7c22b988b47462e3b17ce0a
MarlonSchumacher <marlon.schumacher@statworx.com>
2020-06-10 13:47:15 +0200

    new groups

--- test_merge.R
+++ test_merge.R
@@ -3,6 +3,6 @@
 dplyr::mutate(x = x44guzzzzg34)
 
 
-dplyr::group_by(df, good_group)
-dplyr::group_by(df, better_group)
-dplyr::group_by(df, even_better_group)
+dplyr::group_by(df, no_group)
+dplyr::group_by(df, best_group)
+dplyr::group_by(df, alternative_group)
